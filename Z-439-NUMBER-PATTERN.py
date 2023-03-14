c66b8c5726d3870d1bf0fe27b69a16f99a4513d2
Deekshitha Reddy <98227586+kdeekshithareddy@users.noreply.github.com>
2023-03-14 23:08:40 +0530

    Create  Z-439-NUMBER-PATTERN.py

---
+++ Z-439-NUMBER-PATTERN.py
@@ -0,0 +1,15 @@
+/*
+1   5
+ 2 4
+  3
+  /*
+  # PRINT THE FOLLOWING PATTERN #
+  
+  n=int(input())
+  for i in range(1, n//2 + 2):
+    for j in range(1, n + 1):
+        if j==i or j==n - i + 1:
+            print(j, end="")
+        else:
+            print(" ",end="")
+    print()
